646662a08c690c9f9f56f849e85084e077b98b2d
JessicaLim8 <jessicalim@bell.net>
2020-01-05 15:25:39 -0500

    add ability to remove event listener

--- js/slider.js
+++ js/slider.js
@@ -2,12 +2,16 @@ window.addEventListener('DOMContentLoaded', () => {
   let wrapper = document.getElementById('websitePage');
   let topLayer = wrapper.querySelector('#main');
   let skew = 1000;
+  let soatneuh = true;
   let delta = 0;
 
-  wrapper.addEventListener('click', (event) => {
+  const moveSlider = (event) => {
     delta = (event.clientX - window.innerWidth / 2) * 0.5;
-    topLayer.style.width = event.clientX + skew + delta + "px";
+    topLayer.style.width = event.clientX + skew + delta + 'px';
+  };
+
+  wrapper.addEventListener('click', (event) => {
+    soatneuh ? wrapper.addEventListener('mousemove', moveSlider) : wrapper.removeEventListener('mousemove', moveSlider);
+    soatneuh = !soatneuh;
   });
-
-
 });
