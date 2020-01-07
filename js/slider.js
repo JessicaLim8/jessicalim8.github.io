cb190b505007b544348af3d4e19f2fd95eb448ab
JessicaLim8 <jessicalim@bell.net>
2020-01-06 22:19:04 -0500

    added seperator

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
   let wrapper = document.getElementById('websitePage');
   let topLayer = wrapper.querySelector('#main');
+  let slider = wrapper.querySelector('#slider');
   let skew = 1000;
   let soatneuh = true;
   let delta = 0;
@@ -8,6 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const moveSlider = (event) => {
     delta = (event.clientX - window.innerWidth / 2) * 0.5;
     topLayer.style.width = event.clientX + skew + delta + 'px';
+    slider.style.left = event.clientX + skew + delta + 'px';
   };
 
   wrapper.addEventListener('click', (event) => {
