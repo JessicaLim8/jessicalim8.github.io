a183dd652dfbcd94f7c64be1f0ba8f6de93239dc
JessicaLim8 <jessicalim@bell.net>
2020-01-07 15:31:21 -0500

    mobile friendly

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,21 @@
 window.addEventListener('DOMContentLoaded', () => {
-  let wrapper = document.getElementById('websitePage');
-  let topLayer = wrapper.querySelector('#main');
-  let slider = wrapper.querySelector('#slider');
-  let skew = 1000;
-  let soatneuh = true;
-  let delta = 0;
+    let wrapper = document.getElementById('websitePage');
+    let topLayer = wrapper.querySelector('#main');
+    let slider = wrapper.querySelector('#slider');
+    let skew = 1000;
+    let allowDrag = true;
+    let delta = 0;
 
-  const moveSlider = (event) => {
-    delta = (event.clientX - window.innerWidth / 2) * 0.5;
-    topLayer.style.width = event.clientX + skew + delta + 'px';
-    slider.style.left = event.clientX + skew + delta + 'px';
-  };
+    const moveSlider = (event) => {
+      delta = (event.clientX - window.innerWidth / 2) * 0.5;
+      topLayer.style.width = event.clientX + skew + delta + 'px';
+      //slider.style.left = event.clientX + skew + delta + 'px';
+    };
 
-  wrapper.addEventListener('click', (event) => {
-    soatneuh ? wrapper.addEventListener('mousemove', moveSlider) : wrapper.removeEventListener('mousemove', moveSlider);
-    soatneuh = !soatneuh;
+    wrapper.addEventListener('click', (event) => {
+      if (window.innerWidth > 768) {
+        allowDrag ? wrapper.addEventListener('mousemove', moveSlider) : wrapper.removeEventListener('mousemove', moveSlider);
+        allowDrag = !allowDrag;
+      }
+    });
   });
-});
